d4929ce7c423f9bca0b4d432f802fe6159ede881
Pharisaeuss <vasyanovich.liza@gmail.com>
2024-10-25 22:05:36 +0300

    Added comments

--- suite.cpp
+++ suite.cpp
@@ -1,13 +1,8 @@
 #include "suite.h"
 #include <cmath> 
-
+/** it's function what returns null*/
 double suite::calculate(double x, int n) {
-    double result = 0.0;
-
-    for (int i = 1; i <= n; ++i) {
-        result += pow(-1, i - 1) * pow(x, i) / i;
-    }
-
-    return result; 
+    return 0; 
 }
+/** some comment */
 
